2960b7e7e679136e103ea432bdc81f564c48c364
Clément Béal <clement.beal@etu.unice.fr>
2016-11-16 14:09:59 +0100

    Ajout métric et macro

---
+++ src/brainfuck/language/Macro.java
@@ -0,0 +1,84 @@
+package brainfuck.language;
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Cette classe gère les macros dans le langage BrainFuck
+ * Une macro aura une syntaxe semblable au C
+ * ex : #define NOM_MACRO code
+ *      #define NOM_MACRO(param1, param2, ...) code (cas avec paramètre)
+ * Dans notre cas, ce sera :
+ *      \@NOM_MACRO(param1,param2, ...) code
+ * @author jamatofu on 04/11/16.
+ */
+public class Macro {
+    private TreeMap<String, String> macro = new TreeMap<String, String>();
+    private String programme;
+
+    /**
+     * Permet de détecter les lignes qui correspondent à une macro
+     * Les renvoie ensuite au découpeur de macro
+     * @param texte le programme en entier
+     */
+    public void findMacro(String texte) {
+        ArrayList<String> toutesLesMacros = new ArrayList<String>();
+        int i = 0;
+        while(texte.charAt(0) == '@') {
+            toutesLesMacros.add(texte.substring(0, texte.indexOf("\n")));
+            texte = supprimerLigneDeTexte(toutesLesMacros.get(toutesLesMacros.size() -1), texte);
+        }
+
+        programme = texte;
+
+        //for(String a : toutesLesMacros) System.out.println(a);
+        decomposerMacro(toutesLesMacros);
+    }
+
+    /**
+     * Découpe dans chaque ligne de macro son nom et son code puis l'insère dans le Map macro
+     * @param macrosADecouper les lignes contenants les macros
+     */
+    public void decomposerMacro(ArrayList<String> macrosADecouper) {
+        for (String mac : macrosADecouper) {
+            mac = mac.replaceFirst("@", "");
+
+            String name = mac.substring(0, mac.indexOf(" "));
+            String code = mac.substring(mac.indexOf(" ") + 1, mac.length());
+
+            macro.put(name, code);
+        }
+    }
+
+    /**
+     * Change les commandes macro dans le programme par les instructions correspondantes
+     * @return le texte modifié
+     */
+    public void remplacerMacroParCode() {
+        for(Entry<String, String> entry : macro.entrySet()) {
+            programme = programme.replaceAll(entry.getKey(), entry.getValue());
+        }
+
+        programme = OperationTexte.transformerInstructionEnSymbole(programme);
+    }
+
+    /**
+     * Supprime une ligne d'un texte
+     * @param ligneASupprimer la ligne à supprimer du texte
+     */
+    public String supprimerLigneDeTexte(String ligneASupprimer, String texte) {
+        return texte.replace(ligneASupprimer + "\n", "");
+    }
+
+    public boolean toutEnOrdre() {
+        System.out.print(programme);
+        Pattern pattern = Pattern.compile("(.*)[^+-](.*)");
+        Matcher matcher = pattern.matcher(programme);
+        return matcher.find();
+    }
+
+    public String getProgramme() { return programme; }
+
+}
