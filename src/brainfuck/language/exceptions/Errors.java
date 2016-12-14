05764be0d769e96ee0cba517a12fbbb74cd80f02
Guillaume Derringer <guillaume.derringer@etu.unice.fr>
2016-12-14 17:12:11 +0100

    gérer les erreurs

--- src/brainfuck/language/exceptions/Errors.java
+++ src/brainfuck/language/exceptions/Errors.java
@@ -15,9 +15,46 @@ public class Errors {
         System.out.println("One of the macros has a reserved name");
     }
 
-    public void handle(FilePathNotFoundException e){}
+    public void handle(CheckFailedException e){
+        System.out.println("Code Error : 4");
+    }
+
+    public void handle(FilePathNotFoundException e){System.out.println(e.toString());}
 
     public void handle(FileNotFoundException e){
         System.out.println("Error Code : 3");
+    }
+
+    public void handle(IsNotACommandException e){
+        errorCode();
+        System.out.println(e.toString());
+    }
+
+    public void handle(IsNotAValidColorException e){
+        errorCode();
+        System.out.println(e.toString());
+    }
+
+    public void handle(MainFlagNotFoundException e){
+        errorCode();
+        System.out.println(e.toString());
+    }
+
+    public void handle(OutOfMemoryException e){ System.out.println(e.toString());}
+
+    public void handle(UnknownFlagsException e){
+        errorCode();
+        System.out.println(e.toString());
+    }
+
+    public void handle(ValueOutOfBoundException e){ System.out.println(e.toString());}
+
+    public void handle(WrongInput e){
+        errorCode();
+        System.out.println(e.toString());
+    }
+
+    private void errorCode(){
+        System.out.println("Error Code : -1");
     }
 }
